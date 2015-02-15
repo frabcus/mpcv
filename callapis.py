bf88a9a8cbb847c94cee06493889fa8c2c475d52
Francis Irving <francis@flourish.org>
2015-02-15 13:42:02 +0000

    Factor out API calling functions to separate module

---
+++ callapis.py
@@ -0,0 +1,38 @@
+def lookup_postcode(postcode):
+    data = requests.get("http://mapit.mysociety.org/postcode/" + postcode).json()
+    if "error" in data:
+        return data
+    return data["areas"][str(data["shortcuts"]["WMC"])]
+
+def lookup_candidates(constituency_id):
+    str_id = str(int(constituency_id))
+
+    data = requests.get("http://yournextmp.popit.mysociety.org/api/v0.1/posts/%s?embed=membership.person" % str_id).json()
+    if "errors" in data:
+        return data
+
+    current_candidate_list = []
+    got_urls = set()
+    for member in data['result']['memberships']:
+        standing_in = member['person_id']['standing_in']
+        if year in standing_in and standing_in[year] != None:
+            if standing_in[year]['post_id'] == str_id:
+                # TODO: remove this got_urls hack which is just there to
+                # remove a duplicate Louise Ellman - have asked on Democracy Club list
+                m = member['person_id']
+                if m['url'] not in got_urls:
+                    current_candidate_list.append({
+                        'id': m['id'],
+                        'name': m['name'],
+                        'email': m['email'],
+                        'party': m['party_memberships'][year]['name']
+                    })
+                    got_urls.add(m['url'])
+
+    # Sort by surname (as best we can -- "Duncan Smith" won't work)
+    # so it is same as on ballot paper. So can get used to it.
+    def surname(candidate):
+        return candidate['name'].split(" ")[-1]
+    return sorted(current_candidate_list, key=surname)
+
+
--- main.py
+++ main.py
@@ -5,47 +5,12 @@ import flask
 import requests
 import json
 
+import remoteapis
+
 year = '2015'
 
 app = flask.Flask(__name__)
 app.secret_key = os.getenv('MPCV_SESSION_SECRET')
-
-def lookup_postcode(postcode):
-    data = requests.get("http://mapit.mysociety.org/postcode/" + postcode).json()
-    if "error" in data:
-        return data
-    return data["areas"][str(data["shortcuts"]["WMC"])]
-
-def lookup_candidates(constituency_id):
-    str_id = str(int(constituency_id))
-
-    data = requests.get("http://yournextmp.popit.mysociety.org/api/v0.1/posts/%s?embed=membership.person" % str_id).json()
-    if "errors" in data:
-        return data
-
-    current_candidate_list = []
-    got_urls = set()
-    for member in data['result']['memberships']:
-        standing_in = member['person_id']['standing_in']
-        if year in standing_in and standing_in[year] != None:
-            if standing_in[year]['post_id'] == str_id:
-                # TODO: remove this got_urls hack which is just there to
-                # remove a duplicate Louise Ellman - have asked on Democracy Club list
-                m = member['person_id']
-                if m['url'] not in got_urls:
-                    current_candidate_list.append({
-                        'id': m['id'],
-                        'name': m['name'],
-                        'email': m['email'],
-                        'party': m['party_memberships'][year]['name']
-                    })
-                    got_urls.add(m['url'])
-
-    # Sort by surname (as best we can -- "Duncan Smith" won't work)
-    # so it is same as on ballot paper. So can get used to it.
-    def surname(candidate):
-        return candidate['name'].split(" ")[-1]
-    return sorted(current_candidate_list, key=surname)
 
 @app.route('/error')
 def error():
@@ -62,7 +27,7 @@ def index():
 @app.route('/set_postcode')
 def set_postcode():
     postcode = flask.request.args.get('postcode')
-    constituency = lookup_postcode(postcode)
+    constituency = remoteapis.lookup_postcode(postcode)
 
     if 'error' in constituency:
         flask.flash(constituency['error'], 'danger')
@@ -89,7 +54,7 @@ def applicants():
     postcode = flask.session['postcode']
     constituency = flask.session['constituency']
 
-    applicants = lookup_candidates(constituency['id'])
+    applicants = remoteapis.lookup_candidates(constituency['id'])
     if 'errors' in applicants:
         flask.flash("Error fetching list of candidates from YourNextMP.", 'danger')
         return flask.redirect(flask.url_for('error'))
