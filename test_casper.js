8137f6a7e0c6f04b8b3f1fc3ad611abb8044ba03
Francis Irving <francis@flourish.org>
2015-03-10 01:38:45 +0000

    Postcode error test

--- test_casper.js
+++ test_casper.js
@@ -5,12 +5,34 @@ var address = casper.cli.get("address");
 var screenshot = 0;
 casper.on("load.finished", function() {
     screenshot += 1;
-    this.capture("screenshots/" + screenshot +  ".png");
+    var name = "screenshots/" + screenshot +  ".png";
+    this.capture(name);
+    // console.log("screenshot: ", name, "url:", casper.getCurrentUrl());
 });
 
-// Tests
+// Tests in detail
 
-casper.test.begin('Postcode lookup', 10, function suite(test) {
+casper.test.begin('Bad postcode entered', 5, function suite(test) {
+    casper.start(address + "candidates", function(result) {
+        test.assertEquals(result.status, 200);
+        test.assertTextExists('Before you vote, look at their CVs!');
+        test.assertTextExists('Debug email enabled');
+
+        this.fill('form[action="/set_postcode"]', { postcode: "moo" }, true);
+    });
+
+
+    casper.waitForUrl(/\/$/, function() {
+        test.assertTextExists('Before you vote, look at their CVs!');
+        test.assertTextExists("Postcode 'MOO' is not valid.");
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+
+casper.test.begin('Postcode lookup and constituency cookie', 13, function suite(test) {
     casper.start(address, function(result) {
         test.assertEquals(result.status, 200);
         test.assertTextExists('Before you vote, look at their CVs!');
@@ -19,7 +41,7 @@ casper.test.begin('Postcode lookup', 10, function suite(test) {
         this.fill('form[action="/set_postcode"]', { postcode: "zz99zz" }, true);
     });
 
-    casper.then(function() {
+    casper.waitForUrl(/candidates$/, function() {
         test.assertTextExists('Democracy Club Test Constituency');
         test.assertTextExists('View CVs of these candidates');
         test.assertTextExists('Sicnarf Gnivri');
@@ -30,14 +52,32 @@ casper.test.begin('Postcode lookup', 10, function suite(test) {
     });
 
     // make sure constituency remembered, and front page redirects back to constituency page
+    casper.thenOpen(address, function() {
+        test.assertTextExists('Democracy Club Test Constituency');
+        test.assertTextExists('View CVs of these candidates');
+
+        this.clickLabel("Change constituency");
+    });
+
+    // "Change constituency" clears the memory of constituency
     casper.then(function() {
-        //r = self.app.get('/', follow_redirects=True)
-        //self.assertEqual(r.status_code, 200)
-        //self.assertIn('Candidates for job of MP', r.get_data(True))
-        //self.assertIn('Democracy Club Test Constituency', r.get_data(True))
+        test.assertTextExists('Before you vote, look at their CVs!');
     });
 
     casper.run(function() {
         test.done();
     });
 });
+
+casper.test.begin('Candidates list only with postcode', 3, function suite(test) {
+    casper.start(address + "candidates", function(result) {
+        test.assertEquals(result.status, 200);
+        test.assertTextExists('Before you vote, look at their CVs!');
+        test.assertTextExists('Debug email enabled');
+    });
+
+    casper.run(function() {
+        test.done();
+    });
+});
+
