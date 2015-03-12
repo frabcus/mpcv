ee869e32de75704f5e412c700836bcdbbca9371c
Francis Irving <francis@flourish.org>
2015-03-12 01:28:04 +0000

    Bring unit tests up to date

--- main_tests.py
+++ main_tests.py
@@ -74,7 +74,7 @@ class MainTestCase(unittest.TestCase):
         self.assertEqual(r.status_code, 200)
         self.assertIn('Hi, Sicnarf Gnivri!', r.get_data(True))
         self.assertIn('<form action="/upload_cv/7777777" method="POST">', r.get_data(True))
-        self.assertIn('<button type="submit" class="btn btn-primary">Confirm email</button>', r.get_data(True))
+        self.assertIn('<button id="confirm_email" type="submit" class="btn btn-primary">Confirm email</button>', r.get_data(True))
         self.assertIn('frabcus@fastmail.fm', r.get_data(True))
 
     def test_upload_cv_error(self):
@@ -102,8 +102,8 @@ class MainTestCase(unittest.TestCase):
         # View the page where the upload form is
         r = self.app.get(confirmation_url)
         self.assertEqual(r.status_code, 200)
-        self.assertIn('Drop your CV in here', r.get_data(True))
-        self.assertIn('data-url="' + confirmation_url + '"', r.get_data(True))
+        self.assertIn('Choose a Word document or PDF', r.get_data(True))
+        self.assertIn('action="' + confirmation_url + '"', r.get_data(True))
 
         # Upload
         rup = self.app.post(confirmation_url, data=dict(
