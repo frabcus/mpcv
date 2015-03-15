ddced53f830b78a38144708e8f1ef1fc2c1d8556
Francis Irving <francis@flourish.org>
2015-03-15 10:15:32 +0000

    Finish script to make Sendy CSV

---
+++ bin/make-no-cv-list.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import csv
+import codecs
+import collections
+import requests
+import urllib.request
+
+sys.path.append(os.getcwd())
+import main
+import identity
+import lookups
+
+main.app.config['SERVER_NAME'] = 'cv.democracyclub.org.uk'
+
+with main.app.app_context():
+
+    url = "https://yournextmp.com/media/candidates.csv"
+    stream = urllib.request.urlopen(url)
+    rows = csv.DictReader(codecs.iterdecode(stream, 'utf-8'))
+    rows = lookups.augment_if_has_cv(main.app.config, list(rows))
+
+    writer = csv.DictWriter(sys.stdout, ['name', 'email', 'id', 'party', 'constituency', 'upload_url'])
+
+    for row in rows:
+        if row['has_cv']:
+            continue
+
+        out = collections.OrderedDict()
+        out['name'] = row['name']
+        out['email'] = row['email']
+        out['id'] = row['id']
+        out['party'] = row['party']
+        out['constituency'] = row['constituency']
+
+        link = identity.generate_upload_url(main.app.secret_key, row['id'])
+        out['upload_url'] = link
+
+        writer.writerow(out)
+
+
+
