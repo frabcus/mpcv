42333793b0a57ed4a0c8f2990c292e6449abf237
Francis Irving <francis@flourish.org>
2015-03-18 00:36:54 +0000

    Add party to MP page

--- main.py
+++ main.py
@@ -49,7 +49,7 @@ def _cache_all_cvs():
 
 @app.route('/')
 def index():
-    recent_cvs = _cache_all_cvs()[0:4]
+    recent_cvs = _cache_all_cvs()[0:2]
     return flask.render_template('index.html', recent_cvs=recent_cvs)
 
 @app.route('/all_cvs/page/<int:page>')
