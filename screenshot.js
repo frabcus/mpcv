6a4d947951cdf8815cc5433c61703f5367423475
Andy Lulham <a.lulham@gmail.com>
2015-03-22 19:18:22 +0000

    Generate CV thumbnails on heroku with phantomjs

---
+++ screenshot.js
@@ -0,0 +1,66 @@
+var system = require('system');
+var page = require('webpage').create();
+
+// Usage: phantomjs screenshot.js <CV URL> <filename>
+
+var cvUrl = system.args[1];
+var filename = system.args[2];
+
+/**
+ * Wait until the test condition is true or a timeout occurs. Useful for waiting
+ * on a server response or for a ui change (fadeIn, etc.) to occur.
+ *
+ * @param testFx javascript condition that evaluates to a boolean,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param onReady what to do when testFx condition is fulfilled,
+ * it can be passed in as a string (e.g.: "1 == 1" or "$('#bar').is(':visible')" or
+ * as a callback function.
+ * @param timeOutMillis the max amount of time to wait. If not specified, 3 sec is used.
+ */
+function waitFor(testFx, onReady, timeOutMillis) {
+    var maxtimeOutMillis = timeOutMillis ? timeOutMillis : 3000, //< Default Max Timout is 3s
+        start = new Date().getTime(),
+        condition = false,
+        interval = setInterval(function() {
+            if ( (new Date().getTime() - start < maxtimeOutMillis) && !condition ) {
+                // If not time-out yet and condition not yet fulfilled
+                condition = (typeof(testFx) === "string" ? eval(testFx) : testFx()); //< defensive code
+            } else {
+                if(!condition) {
+                    // If condition still not fulfilled (timeout but condition is 'false')
+                    console.log("'waitFor()' timeout");
+                    phantom.exit(1);
+                } else {
+                    // Condition fulfilled (timeout and/or condition is 'true')
+                    typeof(onReady) === "string" ? eval(onReady) : onReady(); //< Do what it's supposed to do once the condition is fulfilled
+                    clearInterval(interval); //< Stop this interval
+                }
+            }
+        }, 250); //< repeat check every 250ms
+};
+
+var url = "http://docs.google.com/viewer?embedded=true&url=" + encodeURIComponent(cvUrl);
+
+console.log("Generating screenshot for: " + url);
+page.open(url, function (status) {
+    if (status !== "success") {
+        console.log("Unable to access network");
+        phantom.exit();
+    } else {
+        page.viewportSize = {width: 400, height: 566};
+        page.includeJs("https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js", function() {
+            waitFor(function () {
+                return page.evaluate(function() {
+                    return $(['role=\"document\"']).length > 0;
+                });
+            }, function() {
+                // wait another 10 seconds (until page num and toolbar disappear)
+                setTimeout(function () {
+                    page.render(filename);
+                    phantom.exit();
+                }, 10000);
+            });
+        });
+    }
+});
