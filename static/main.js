48188d00dc8c4f71d45dfebc532783b160d5c08b
Francis Irving <francis@flourish.org>
2015-03-27 02:07:12 +0000

    /show_cv had extra grey border

--- static/main.js
+++ static/main.js
@@ -2,9 +2,9 @@ $(function () {
 
   // Cope with top navigation wrapping
   // http://stackoverflow.com/a/20197508
-  $(document.body).css('padding-top', $('#topnavbar').height());
+  $(document.body).css('padding-top', $('#topnavbar').height() - 10);
   $(window).resize(function(){
-      $(document.body).css('padding-top', $('#topnavbar').height());
+      $(document.body).css('padding-top', $('#topnavbar').height() - 10);
   });
 
   // File upload URL button
