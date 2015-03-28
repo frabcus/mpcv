cea86e8a37d49d14e00a70957743dfe2c0e69576
Francis Irving <francis@flourish.org>
2015-03-28 14:40:33 +0000

    Add semicolons
    As vim shows errors on them, and for consistency as had somewhere

--- static/main.js
+++ static/main.js
@@ -9,44 +9,44 @@ $(function () {
 
   // File upload URL button
   $(".files").change(function() {
-    var btn = $(this)
-    btn.parent().parent().submit()
+    var btn = $(this);
+    btn.parent().parent().submit();
     // This is a bit complicated - have to disable the invisible button,
     // change the text of the span above, and set the visible (bootstrap)
     // disabled attribute on the span button lookalike parent.
     // (Note that we can't use jquery's button.reset here due to this complexity)
-    btn.prop('disabled', true)
-    btn.prev().attr("x-text-before", btn.prev().text())
-    btn.prev().text("Uploading CV...")
-    btn.parent().attr("disabled", "")
-  })
+    btn.prop('disabled', true);
+    btn.prev().attr("x-text-before", btn.prev().text());
+    btn.prev().text("Uploading CV...");
+    btn.parent().attr("disabled", "");
+  });
 
   // Reset buttons, on Firefox this is needed when doing back button.
   // http://duckranger.com/2012/01/double-submit-prevention-disabled-buttons-firefox-and-the-back-button/
   $(window).on("pageshow.files", function() {
     $(".files").each(function() {
-      var btn = $(this)
-      var text_before = btn.prev().attr("x-text-before")
-      btn.prev().text(text_before)
-      btn.prop('disabled', false)
-      btn.parent().removeAttr("disabled")
+      var btn = $(this);
+      var text_before = btn.prev().attr("x-text-before");
+      btn.prev().text(text_before);
+      btn.prop('disabled', false);
+      btn.parent().removeAttr("disabled");
 
       // Clearing the value is needed on all browsers so "change" event still fires
       // if they select the same document again.
-      btn.val("")
-    })
+      btn.val("");
+    });
 
-  })
+  });
 
   // Used on send email page (initially)
-  var initiator = ""
-  $(".button-disabling-form .btn").click(function() { initiator = this.id });
+  var initiator = "";
+  $(".button-disabling-form .btn").click(function() { initiator = this.id; });
   $(".button-disabling-form").on('submit', function() {
-    $("#" + initiator).button('loading')
-  })
+    $("#" + initiator).button('loading');
+  });
   // Needed for Firefox / back button
   $(window).on("pageshow.button-disabling-form", function() {
-    $(".button-disabling-form .btn").button('reset').prop('disabled', false)
+    $(".button-disabling-form .btn").button('reset').prop('disabled', false);
   });
 
   // Custom Tweet button
