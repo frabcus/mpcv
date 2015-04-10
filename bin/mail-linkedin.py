b6a35a3d7576d83a1240699d75913cc9b7972f7a
Francis Irving <francis@flourish.org>
2015-04-10 21:13:05 +0100

    Live script!

--- bin/mail-linkedin.py
+++ bin/mail-linkedin.py
@@ -5,6 +5,7 @@ import os
 import collections
 
 import flask_mail
+import boto.s3.key
 
 sys.path.append(os.getcwd())
 import main
@@ -13,9 +14,14 @@ import lookups
 
 main.app.config['SERVER_NAME'] = 'cv.democracyclub.org.uk'
 
+mailed_hash = lookups._hash_by_prefix(main.app.config, "mailed/linkedin/")
+
 with main.app.app_context():
     for constituency in lookups.all_constituencies(main.app.config):
         for candidate in constituency:
+            if candidate['id'] in mailed_hash:
+                print("already mailed", candidate)
+                continue
             if candidate['has_cv']:
                 continue
             if not candidate['linkedin_url']:
@@ -30,13 +36,14 @@ with main.app.app_context():
 We're helping Parliamentary candidates share their CV
 with voters.
 
-To make it easy, you can give us permission to use your
-LinkedIn page as your CV.
+To make it easy, you can let us use your LinkedIn
+profile as your CV.
 
 Just press "reply", and say "yes"!
 
-You might first want to quickly check your LinkedIn page is up
-to date, and has your full career and education history.
+You might first want to quickly check your LinkedIn
+page is up to date, and has your full career and
+education history.
 
 {linkedin_url}
 
@@ -52,20 +59,23 @@ a Word or PDF document, and go here:
 
             print("=========================\n" + body)
 
-            candidate['email'] = 'frabcus@fastmail.fm'
+            #candidate['email'] = 'frabcus@fastmail.fm'
 
             msg = flask_mail.Message(body=body,
                     subject="Easily share your LinkedIn CV with your voters!",
                     sender=("Democracy Club CVs", "cv@democracyclub.org.uk"),
                     recipients=[
-                        candidate['name'],
-                        candidate['email']
+                        (candidate['name'], candidate['email'])
                     ]
                   )
 
-            with main.app.app_context():
-                main.mail.send(msg)
+            main.mail.send(msg)
 
-            sys.exit(0)
+            # record sent
+            bucket = lookups._get_s3_bucket(main.app.config)
+            key = boto.s3.key.Key(bucket)
+            key.key = "mailed/linkedin/" + str(candidate['id']) + ".sent"
+            key.set_contents_from_string("sent")
 
+            sys.exit(1)
 
