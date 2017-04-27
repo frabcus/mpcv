a35e41dffc8085505867e8f610a92c08af685a77
Francis Irving <francis@flourish.org>
2017-04-27 19:34:55 +0100

    Better copy for archive. Only link from About.

--- elections.py
+++ elections.py
@@ -1,4 +1,4 @@
 # Election that CVs can be submitted for now
 current_election = 'parl.2017-06-08'
-current_election_name = 'General Election 2017'
+current_election_name = 'UK General Election 2017'
 
