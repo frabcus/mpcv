628d6a89b0eac1195899085eca4f51885c4b9132
Francis Irving <francis@flourish.org>
2019-11-10 22:32:43 +0000

    Update to 2019 election

--- elections.py
+++ elections.py
@@ -1,4 +1,4 @@
 # Election that CVs can be submitted for now
-current_election = 'parl.2017-06-08'
-current_election_name = 'UK General Election 2017'
+current_election = 'parl.2019-12-12'
+current_election_name = 'UK General Election 2019'
 
